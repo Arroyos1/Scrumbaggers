1f13d0773089fd620e592848af66086e2f3d86c5
BrianA <bdalcorn@outlook.com>
2016-08-02 12:28:16 +0200

    Created Scraps Points

---
+++ Scrumbaggers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using System . Collections . Generic;
+using UnityEngine . UI;
+
+public class GameManager : MonoBehaviour
+{
+     public float levelStartDelay = 2f;
+     public float turnDelay = 0.1f;
+     public static GameManager instance = null;
+     public BoardManager boardScript;
+     public int playerFoodPoints = 100;
+     public int playerScrapPoints = 0;
+     [HideInInspector]
+     public bool player1Turn = true;
+     [HideInInspector]
+     public bool player2Turn = false;
+     [HideInInspector]
+     public bool enemy1Turn = false;
+     [HideInInspector]
+     public bool enemy2Turn = false;
+
+     private Text levelText;
+     private GameObject levelImage;
+     private int level = 1;
+     private List<Enemy> enemies;
+     private List<Enemy2> enemies2;
+     private bool enemy1Moving;
+     private bool enemy2Moving;
+     private bool doingSetup;
+
+	void Awake ()
+     {
+          if ( instance == null )
+               instance = this;
+          else if ( instance != this )
+               Destroy ( gameObject );
+
+          DontDestroyOnLoad ( gameObject );
+          enemies = new List<Enemy> ( );
+          enemies2 = new List<Enemy2> ( );
+          boardScript = GetComponent<BoardManager> ( );
+          InitGame ( );
+
+     }
+
+     private void OnLevelWasLoaded (int index)
+     {
+          level++;
+
+          InitGame ( );
+     }	
+     void InitGame()
+     {
+          doingSetup = true;
+
+          levelImage = GameObject . Find ( "LevelImage" );
+          levelText = GameObject . Find ( "LevelText" ) . GetComponent<Text> ( );
+          levelText . text = "Day" + level;
+          levelImage . SetActive ( true );
+          Invoke ( "HideLevelImage" , levelStartDelay );
+
+          enemies . Clear ( );
+          enemies2 . Clear ( );
+          boardScript . SetupScene ( level );
+     }
+
+     private void HideLevelImage()
+     {
+          levelImage . SetActive (false);
+          doingSetup = false;
+     }
+
+     public void GameOver ()
+     {
+          levelText . text = "After " + level + " days, you starved.";
+          levelImage . SetActive( true );
+          enabled = false;
+     } 
+     
+     void Update()
+     {
+          if ( player1Turn || player2Turn || enemy1Moving || enemy2Moving || doingSetup )
+               return;
+          else if ( enemy1Turn )
+                    StartCoroutine ( MoveEnemies1 ( ) );
+          else if ( enemy2Turn )
+                    StartCoroutine ( MoveEnemies2 ( ) );
+     }
+
+     public void AddEnemyToList(Enemy script)
+     {
+          enemies . Add ( script );
+     }
+
+     public void AddEnemy2ToList (Enemy2 script)
+     {
+          enemies2 . Add ( script );
+     }
+
+     IEnumerator MoveEnemies1 ( )
+     {
+          enemy1Moving = true;
+          yield return new WaitForSeconds ( turnDelay );
+
+          if (enemies.Count == 0 && enemies2.Count == 0)
+          {
+               yield return new WaitForSeconds ( turnDelay );
+          }
+
+          for(int i = 0 ; i < enemies. Count ; i++ )
+          {
+               enemies [ i ] . MoveEnemy1 ( );
+               yield return new WaitForSeconds ( enemies [ i ] . moveTime );
+          }
+
+          enemy1Moving = false;
+          enemy1Turn = false;
+          player1Turn = true;
+     }
+
+     IEnumerator MoveEnemies2 ( )
+     {
+          enemy2Moving = true;
+          yield return new WaitForSeconds ( turnDelay );
+
+          if ( enemies . Count == 0 && enemies2 . Count == 0 )
+          {
+               yield return new WaitForSeconds ( turnDelay );
+          }
+
+          for ( int i = 0 ; i < enemies2 . Count ; i++ )
+          {
+               enemies2 [ i ] . MoveEnemy2 ( );
+               yield return new WaitForSeconds ( enemies2 [ i ] . moveTime );
+          }
+
+          enemy2Moving = false;
+          enemy2Turn = false;
+          player2Turn = true;
+     }
+}
---
+++ Scrumbaggers/Assets/Scripts/Player2.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine. SceneManagement;
+using UnityEngine . UI;
+
+public class Player2 : MovingObject
+{
+     public int wallDamage = 1;
+     public int pointsPerFood = 10;
+     public int pointsPerSoda = 20;
+     public float restartLevelDelay = 1f;
+     public Text foodText2;
+     public Text scrapText2;
+     public AudioClip moveSound1;
+     public AudioClip moveSound2;
+     public AudioClip eatSound1;
+     public AudioClip eatSound2;
+     public AudioClip drinkSound1;
+     public AudioClip drinkSound2;
+     public AudioClip gameOverSound;
+
+     private Animator animator;
+     private int food;
+     private int scraps;
+
+	protected override void Start ()
+     {
+          animator = GetComponent<Animator> ( );
+
+          food = GameManager . instance . playerFoodPoints;
+          scraps = GameManager . instance . playerScrapPoints;
+
+          foodText2 . text = "Food: " + food;
+          scrapText2 . text = "Scraps: " + scraps;
+
+          base . Start ( );
+	}
+	
+     private void OnDisable ()
+     {
+          GameManager . instance . playerFoodPoints = food;
+     }
+
+	void Update ()
+     {
+          if ( !GameManager . instance . player2Turn ) return;
+
+          int horizontal = 0;
+          int vertical = 0;
+
+          horizontal = ( int ) Input . GetAxisRaw ( "Horizontal2" );
+          vertical = ( int ) Input . GetAxisRaw ( "Vertical2" );
+
+          if ( horizontal != 0)
+               vertical = 0;
+
+          if ( horizontal != 0 || vertical != 0 )
+               AttemptMove<Wall> ( horizontal , vertical );
+	}
+
+     protected override void AttemptMove <T>  (int xDir, int yDir)
+     {
+          food--;
+          foodText2 . text = "Food: " + food;
+
+          base . AttemptMove <T> ( xDir , yDir );
+
+          RaycastHit2D hit;
+          if (Move (xDir, yDir, out hit))
+          {
+               SoundManager . instance . RandomizeSfx ( moveSound1 , moveSound2 );
+          }
+
+          CheckIfGameOver ( );
+
+          GameManager . instance . player2Turn = false;
+          GameManager . instance . enemy1Turn =true;          
+     }
+
+     private void OnTriggerEnter2D (Collider2D other)
+     {
+          if (other.tag == "Exit")
+          {
+               Invoke ( "Restart" , restartLevelDelay );
+               enabled = false;
+          }
+          else if (other.tag == "Food")
+          {
+               food += pointsPerFood;
+               foodText2 . text = "+" + pointsPerFood + " Food: " + food;
+               SoundManager . instance . RandomizeSfx ( eatSound1 , eatSound2 );
+               other . gameObject . SetActive ( false );
+          }
+          else if(other.tag == "Soda")
+          {
+               food += pointsPerSoda;
+               foodText2 . text = "+" + pointsPerSoda + " Food: " + food;
+               SoundManager . instance . RandomizeSfx ( drinkSound1 , drinkSound2 );
+               other . gameObject . SetActive ( false );
+          }
+     }
+
+     protected override void OnCantMove <T> (T component)
+     {
+          Wall hitWall = component as Wall;
+          hitWall . DamageWall ( wallDamage );
+          animator . SetTrigger ( "player2Chop" );
+          scraps++;
+          scrapText2 . text = "Scraps: " + scraps;
+     }
+
+     private void Restart()
+     {
+          SceneManager . LoadScene ( "Main" );
+     }
+
+     public void LoseFood (int loss)
+     {
+          animator . SetTrigger ( "player2Hit" );
+          food -= loss;
+          foodText2. text = "-" + loss + " Food: " + food;
+          CheckIfGameOver ( );
+     }
+
+     private void CheckIfGameOver()
+     {
+          if ( food <= 0 )
+          {
+               SoundManager . instance . PlaySingle ( gameOverSound );
+               SoundManager . instance . musicSource . Stop ( );
+               GameManager . instance . GameOver ( );
+          }               
+     }
+}
